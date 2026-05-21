19d70112d283d444040fd300baa75c8b999538f5
ebrambegoo <ebrahim.assadi@gmail.com>
2026-05-22 00:29:22 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = "توکن_واقعی_تو"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await update.message.reply_text("سلام 👋 ربات فعاله!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await update.message.reply_text(update.message.text)
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()

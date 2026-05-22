d83ee028bb9693a67e8aa3f183b91035093b2c58
ebrambegoo <ebrahim.assadi@gmail.com>
2026-05-22 16:40:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+if TOKEN is None:
+    print("BOT_TOKEN is not set!")
+    exit()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام... به ابرام‌بگو خوش اومدین !")
@@ -14,4 +19,5 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
+print("Bot is running...")
 app.run_polling()

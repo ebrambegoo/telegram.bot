ac824a56b0991bfbc36f3763b3a0cc0dc204f247
ebrambegoo <ebrahim.assadi@gmail.com>
2026-05-22 16:10:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = 8598100011:AAF53-MTFRPoVeph3rCSz-_WgT44DvwXPpg
+TOKEN = 8598100011: "8598100011:AAF53-MTFRPoVeph3rCSz-_WgT44DvwXPpg"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text (سلام; به ابرام‌بگو خوش اومدین)
+await update.message.reply_text ("سلام; به ابرام‌بگو خوش اومدین")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text(update.message.text)
